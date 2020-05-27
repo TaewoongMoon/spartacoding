293c4977c41b3e1d9d0fb6902f1fef579f3d5659
TaewoongMoon <taewoongmoon1@gmail.com>
2020-05-27 11:35:22 +0000

    test

---
+++ main.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient
+url = 'mongodb://Moony:jullie6096@ds011459.mlab.com:11459/heroku_nxq5fr78?retryWrites=false'
+client = MongoClient(url)
+db = client['heroku_nxq5fr78']
+collection = db['test']
+
+#collection.insert_one({'a':'b'})
+
+rows = collection.find({})
+for row in rows:
+    print(row)
